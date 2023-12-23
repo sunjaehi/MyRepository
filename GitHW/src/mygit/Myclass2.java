d1b94c421773f45334dc8f0202f402608967ac64
Lee Sunjae <sunjae7944@naver.com>
2023-12-23 21:31:40 +0900

    First commit in dev2 branch

--- GitHW/src/mygit/Myclass2.java
+++ GitHW/src/mygit/Myclass2.java
@@ -4,6 +4,7 @@ public class Myclass2 {
 	public static void main(String args[]) {
 		System.out.println("Hello, Eclipse");
 		System.out.println("Second Version");
+		System.out.println("Everyone Say Hello");
 	}
 	
 
